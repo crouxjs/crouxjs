bbd2db81ae58165cea1a7b809c2facc486bc1a70
unknown <thraverdy@gmail.com>
2020-07-26 23:17:23 +0200

    making library complex object while 'use'

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import shelljs from "shelljs";
 interface Library {
 	[key: string]: ArgCallback;
 }
-export const library: Library = {};
+
+export let library: Library = {};
 
 export const exec = shelljs.exec;
 
@@ -19,9 +20,43 @@ export function execPromise(command: string, options: shelljs.ExecOptions) {
 declare type ArgCallback = (args: string[]) => void;
 
 export function use(path: string, exec: ArgCallback) {
-	library[path] = exec;
+	const pathDeepObject = makeExecuteObject(path, exec);
+	library = {
+		...library,
+		...pathDeepObject,
+	};
 }
 
 export function build() {
 	console.error("Not implemented yet !");
+}
+
+function makeExecuteObject(path: string, exec: ArgCallback): Library {
+	let pathDeepObject = makeDeepObject(path);
+	let args = path.split(" ");
+	let node = getToDeepestObject(pathDeepObject, args);
+	node[args[args.length - 1]] = exec;
+	return pathDeepObject;
+}
+
+function getToDeepestObject(pathDeepObject: any, args: string[]): Library {
+	let node = pathDeepObject;
+	for (let i = 0; i < args.length - 1; i++) {
+		node = node[args[i]];
+	}
+	return node;
+}
+
+interface RecursiveObject {
+	[key: string]: RecursiveObject;
+}
+
+function makeDeepObject(path: string): any {
+	let src: RecursiveObject = {};
+	let node = src;
+	path.split(" ").forEach((value) => {
+		node[value] = {};
+		node = node[value];
+	});
+	return src;
 }
