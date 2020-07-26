a15c2e3ca61ecae3d0cd0a9a9435175ad887f9ec
unknown <thraverdy@gmail.com>
2020-07-26 22:04:51 +0200

    rework de tout mon truc, petit callback des familles

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,27 @@
 import shelljs from "shelljs";
-import { isObject } from "util";
-import { ChildProcess } from "child_process";
-export const shell = shelljs;
 
-export interface UseFonction {
-	(path: string, command: string): Promise<unknown>;
-	(path: string, command: string, options: shelljs.ExecOptions): Promise<
-		unknown
-	>;
-	(path: string, command: string, callback: shelljs.ExecCallback): void;
-	(
-		path: string,
-		command: string,
-		options: shelljs.ExecOptions,
-		callback: shelljs.ExecCallback,
-	): void;
+interface Library {
+	[key: string]: ArgCallback;
 }
+export const library: Library = {};
 
-export const use: UseFonction = function (
-	path: string,
-	command: string,
-	options?: any,
-	callback?: shelljs.ExecCallback,
-): any {
-	if (options == null) {
-		return _usePromise(command);
-	} else if (isObject(options)) {
-		if (callback) {
-			return _useCallback(command, callback, options);
-		} else {
-			return _usePromise(command, options);
-		}
-	} else {
-		return _useCallback(command, (callback = options));
-	}
-};
+export const exec = shelljs.exec;
 
-function _usePromise(
-	command: string,
-	options: shelljs.ExecOptions = {},
-): Promise<unknown> {
-	console.log("using promise !");
+export function execPromise(command: string, options: shelljs.ExecOptions) {
 	return new Promise((resolve, reject) => {
-		shell.exec(command, options, (code, stdout, stderr) => {
+		exec(command, options, (code, stdout, stderr) => {
 			if (code != 0) reject(stderr);
 			else resolve(stdout);
 		});
 	});
 }
 
-function _useCallback(
-	command: string,
-	callback: shelljs.ExecCallback,
-	options: shelljs.ExecOptions = {},
-): ChildProcess {
-	console.log("using callback !");
-	return shell.exec(command, options, callback);
+declare type ArgCallback = (args: string[]) => void;
+
+export function use(path: string, exec: ArgCallback) {
+	library[path] = exec;
+}
+
+export function build() {
+	console.error("Not implemented yet !");
 }
