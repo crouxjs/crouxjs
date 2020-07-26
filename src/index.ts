e240654436a7df81208131c722c8a8de889ae150
unknown <thraverdy@gmail.com>
2020-07-26 17:28:00 +0200

    creation d'un use premiere edition !

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,53 @@
-export class Command {
-	public path: string | undefined;
-	public args: Array<string> | undefined;
+import shelljs from "shelljs";
+import { isObject } from "util";
+export const shell = shelljs;
 
-	constructor() {}
+export interface UseFonction {
+	(path: string, command: string): Promise<any>;
+	(path: string, command: string, options: shelljs.ExecOptions): Promise<any>;
+	(path: string, command: string, callback: shelljs.ExecCallback): void;
+	(
+		path: string,
+		command: string,
+		options: shelljs.ExecOptions,
+		callback: shelljs.ExecCallback,
+	): void;
+}
+
+export const use: UseFonction = function (
+	path: string,
+	command: string,
+	options?: any,
+	callback?: shelljs.ExecCallback,
+): any {
+	if (options == null) {
+		return usePromise(command);
+	} else if (isObject(options)) {
+		if (callback) {
+			useCallback(command, callback, options);
+		} else {
+			return usePromise(command, options);
+		}
+	} else {
+		useCallback(command, (callback = options));
+	}
+};
+
+function usePromise(command: string, options: shelljs.ExecOptions = {}) {
+	console.log("using promise !");
+	return new Promise((resolve, reject) => {
+		shell.exec(command, options, (code, stdout, stderr) => {
+			if (code != 0) reject(stderr);
+			else resolve(stdout);
+		});
+	});
+}
+
+function useCallback(
+	command: string,
+	callback: shelljs.ExecCallback,
+	options: shelljs.ExecOptions = {},
+) {
+	console.log("using callback !");
+	shell.exec(command, options, callback);
 }
