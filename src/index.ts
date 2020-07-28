7a055da04eec9e07916bf6e85640cfdaf4f171b4
unknown <thraverdy@gmail.com>
2020-07-28 23:11:21 +0200

    editorconfig + eslint details + better test names

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
-import shelljs from "shelljs";
+import shelljs from 'shelljs';
 
 class Crouxjs {
-	library: Library = {};
+  library: Library = {};
 
-	exec = shelljs.exec;
+  exec = shelljs.exec;
 
-	execPromise(command: string, options: shelljs.ExecOptions) {
-		return new Promise((resolve, reject) => {
-			this.exec(command, options, (code, stdout, stderr) => {
-				if (code != 0) reject(stderr);
-				else resolve(stdout);
-			});
-		});
-	}
+  execPromise(command: string, options: shelljs.ExecOptions) {
+    return new Promise((resolve, reject) => {
+      this.exec(command, options, (code, stdout, stderr) => {
+        if (code !== 0) reject(stderr);
+        else resolve(stdout);
+      });
+    });
+  }
 
-	use(command: string, exec: ArgCallback) {
-		const pathDeepObject = makeExecuteObject(command, exec);
-		this.library = {
-			...this.library,
-			...pathDeepObject,
-		};
-	}
+  use(command: string, exec: ArgCallback) {
+    const pathDeepObject = makeExecuteObject(command, exec);
+    this.library = {
+      ...this.library,
+      ...pathDeepObject,
+    };
+  }
 
-	build(folder: string) {
-		console.error("Not implemented yet !");
-	}
+  // build(folder: string) {
+  //   console.error('Not implemented yet !');
+  // }
 }
 
 const croux = () => new Crouxjs();
@@ -32,37 +32,37 @@ const croux = () => new Crouxjs();
 export = croux;
 
 interface Library {
-	[key: string]: ArgCallback;
+  [key: string]: ArgCallback;
 }
 
 type ArgCallback = (args: string[]) => void;
 
 function makeExecuteObject(command: string, exec: ArgCallback): Library {
-	let pathDeepObject = makeDeepObject(command);
-	let args = command.split(" ");
-	let node = getToDeepestObject(pathDeepObject, args);
-	node[args[args.length - 1]] = exec;
-	return pathDeepObject;
+  const pathDeepObject = makeDeepObject(command);
+  const args = command.split(' ');
+  const node = getToDeepestObject(pathDeepObject, args);
+  node[args[args.length - 1]] = exec;
+  return pathDeepObject;
 }
 
 function getToDeepestObject(pathDeepObject: any, args: string[]): Library {
-	let node = pathDeepObject;
-	for (let i = 0; i < args.length - 1; i++) {
-		node = node[args[i]];
-	}
-	return node;
+  let node = pathDeepObject;
+  for (let i = 0; i < args.length - 1; i += 1) {
+    node = node[args[i]];
+  }
+  return node;
 }
 
 interface RecursiveObject {
-	[key: string]: RecursiveObject;
+  [key: string]: RecursiveObject;
 }
 
 function makeDeepObject(command: string): any {
-	let nodeTree: RecursiveObject = {};
-	let node = nodeTree;
-	command.split(" ").forEach((value) => {
-		node[value] = {};
-		node = node[value];
-	});
-	return nodeTree;
+  const nodeTree: RecursiveObject = {};
+  let node = nodeTree;
+  command.split(' ').forEach((value) => {
+    node[value] = {};
+    node = node[value];
+  });
+  return nodeTree;
 }
--- test/library.test.js
+++ test/library.test.js
@@ -1,24 +1,28 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line import/no-unresolved
 const croux = require('crouxjs');
 
-test('Library is empty at the beginning', () => {
-  const app = croux();
+let app;
+
+beforeEach(() => {
+  app = croux();
+});
+
+test('Library is empty WHEN creating a new app instance', () => {
   expect(Object.keys(app.library)).toHaveLength(0);
 });
 
-test('Add one command to the library when calling croux.use([...]) once', () => {
-  const app = croux();
+test('Add one command to the library WHEN calling app.use([...]) once', () => {
   app.use('test', () => {});
   expect(Object.keys(app.library)).toHaveLength(1);
 });
 
-test('Added command can be accessed in the library when using croux.use([...])', () => {
-  const app = croux();
+test('Added command can be accessed AFTER calling app.use([...])', () => {
   app.use('test', () => {});
   expect(app.library).toHaveProperty('test');
 });
 
-test('use([...]) given callback can be executed', () => {
-  const app = croux();
+test('use([...]) given callback can be executed FROM the library ', () => {
   app.use('test', () => 'Hello');
   expect(app.library).toHaveProperty('test');
   const result = app.library.test();
