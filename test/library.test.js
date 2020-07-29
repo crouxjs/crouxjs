fbea739ca84ca87bce04d7735ad755cf33961bed
Thomas Raverdy <traverdy@norsys.fr>
2020-07-29 13:36:08 +0200

    toujours plus de tests et de config, bien on avancera :(

--- test/library.test.js
+++ test/library.test.js
@@ -2,19 +2,47 @@
 // eslint-disable-next-line import/no-unresolved
 const croux = require('crouxjs');
 
-let app = croux(); // Needed for intellisense
+let app = croux(); // Needed for VS code intellisense
 
 beforeEach(() => {
   app = croux();
+});
+
+test('Library is not undefined WHEN creating a new app instance', () => {
+  expect(app.library).not.toBeUndefined();
 });
 
 test('Library is empty WHEN creating a new app instance', () => {
   expect(Object.keys(app.library)).toHaveLength(0);
 });
 
-test('Add one command to the library WHEN calling app.use([...]) once', () => {
+test('Library has 1 command WHEN calling app.use([...]) once', () => {
   app.use('test', () => {});
   expect(Object.keys(app.library)).toHaveLength(1);
+});
+
+test('Library has 1 command WHEN calling app.use([...]) twice with the exact same command', () => {
+  app.use('test', () => {});
+  app.use('test', () => {});
+  expect(Object.keys(app.library)).toHaveLength(1);
+});
+
+test('Library has 2 commands WHEN calling app.use([...]) twice using differents names', () => {
+  app.use('start', () => {});
+  app.use('stop', () => {});
+  expect(Object.keys(app.library)).toHaveLength(2);
+});
+
+test('Library has 1 entry WHEN calling app.use([...]) twice names with same first keyword', () => {
+  app.use('test start', () => {});
+  app.use('test stop', () => {});
+  expect(Object.keys(app.library.test)).toHaveLength(2);
+});
+
+test('Library has 2 sub-entries WHEN calling app.use([...]) twice names with same first keyword', () => {
+  app.use('test start', () => {});
+  app.use('test stop', () => {});
+  expect(Object.keys(app.library.test)).toHaveLength(2);
 });
 
 test('Added command can be accessed AFTER calling app.use([...])', () => {
@@ -24,7 +52,12 @@ test('Added command can be accessed AFTER calling app.use([...])', () => {
 
 test('use([...]) given callback can be executed FROM the library ', () => {
   app.use('test', () => 'Hello');
-  expect(app.library).toHaveProperty('test');
-  const result = app.library.test();
-  expect(result).toBe('Hello');
+  expect(app.library.test()).toBe('Hello');
+});
+
+test('use([...]) given callback can be executed FROM the library ', () => {
+  app.use('test start', () => 'Start');
+  app.use('test stop', () => 'Stop');
+  expect(app.library.test.start()).toBe('Start');
+  expect(app.library.test.stop()).toBe('Stop');
 });
